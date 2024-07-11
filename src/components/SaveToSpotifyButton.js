4fc0b4d8923f78e83b89537a998330404fcbedfa
MarioBlasi <mblasi509@gmail.com>
2024-07-11 13:47:38 +0100

    buttonSearch

---
+++ src/components/SaveToSpotifyButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SaveToSpotifyButton.css';
+
+const SaveToSpotifyButton = () => {
+  return (
+    <button className="save-to-spotify-button">
+      Save To Spotify
+    </button>
+  );
+};
+
+export default SaveToSpotifyButton;
---
+++ src/components/SearchButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchButton.css'; 
+
+const SearchButton = () => {
+  return (
+    <button className="search-button">
+      Search
+    </button>
+  );
+};
+
+export default SearchButton;
